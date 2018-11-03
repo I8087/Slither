a240fbe9db6ec8fe032530e364ae7ad95f34204c
I8087 <i8086@protonmail.com>
2018-11-03 19:08:54 +0900

    Update Windows Build Script

--- build-win.cmd
+++ build-win.cmd
@@ -1,6 +1,6 @@
 @echo off
 rem A simple windows build script for Slither. Requires PyInstaller
-pyinstaller --clean --onefile slither_cmd.py
+pyinstaller --clean -n slither -F slither_cmd.py
 rmdir /S /Q build
 rmdir /S /Q __pycache__
 pause
